66ee820e89020470821b44183cf40c2aff6729dc
Eran Finkel <eran.f@taboola.com>
2019-01-27 23:01:35 +0200

    #7

--- BqExecuter.py
+++ BqExecuter.py
@@ -6,16 +6,11 @@ class BqExecuter(object):
 
     def execute(self, query):
         try:
-            #command = ['bq', 'query', '--use_legacy_sql=false', '--dry_run', r'select pv.userPageviewCounters.counterNames from `taboola-data.pageviews.pageviews_20190120`']
             command = ['bq', 'query', '--use_legacy_sql=false', '--dry_run', query]
 
             output = subprocess.Popen(command, shell=False, stdout=subprocess.PIPE).communicate()[0]
-            size_bytes = output.split()[14]
+            size_bytes = int(output.split()[14]) # replace with output parser
             return size_bytes
-            #print size_bytes
-            # for line in output.splitlines():
-            #     size = line.split('\t')[0]
-            #     directory = line.split('\t')[1]
-            #     self._directory_size_map[directory] = int(size)
+
         except (OSError, ValueError, TypeError, IndexError) as e:
             raise Exception('Failed to execute command. reason: {0}'.format(e.message))
--- bq-field-size-analyzer.py
+++ bq-field-size-analyzer.py
@@ -1,3 +1,8 @@
+import argparse
+import datetime
+import json
+import subprocess
+
 from BqExecuter import BqExecuter
 from BqQueryBuilder import BqQueryBuilder
 from ColumnMetadata import ColumnMetadata
@@ -9,34 +14,59 @@ def main():
 
 
     # parse input
-    # take date list
-    # for each:
-    ## convert to table name
-    ## Get Table metadata (byte size, num rows)
-    ## get table schema via bq as json file
-    ## parse json Schema
+    args = parseArgs()
+    if not args.from_date:
+        today = datetime.date.today()
+        from_date = (today - datetime.timedelta(days=1)).strftime('%Y%m%d')
+        print from_date
+    else:
+        from_date = datetime.datetime.strptime(args.from_date, '%Y%m%d')
 
-    schemaParser = JsonSchemaParser('pageview-simple-json-schema.json')
-    schema = schemaParser.parse()
+    if not args.to_date:
+        to_date = from_date
+        print to_date
+    else:
+        to_date = datetime.datetime.strptime(args.to_date, '%Y%m%d')
 
-    ## prepare column list based on cmd line args and pass them to a class to:
-    ## this class creates queries, execute and builds output dictionary(?)
+    date_list = []
+    while from_date <= to_date:
+        date_list.append(from_date.strftime('%Y%m%d'))
+        from_date = from_date + datetime.timedelta(days=1)
 
-    # analyze a single column name (fully qualified) with childs
-    # for key, value in schemaParser.column_name_dictionary.iteritems():
-    #     if key.startswith('pv.requests.servedItems'):
-    #         pass
+    for date in date_list:
 
-    # prepare list of columns
-    columns = map(lambda (name, col): ColumnMetadata(name, col), schemaParser.column_name_dictionary.iteritems())
+        ## convert to table name
+        ## Get Table metadata (byte size, num rows)
+        ## get table schema via bq as json file
+        # bq show --format=json taboola-data:pageviews.pageviews_20190122
+        table_name = args.project + ':' + args.dataset + '.' + args.table_prefix + '_' + date
+        command = ['bq', 'show', '--format=json', table_name]
+        output = subprocess.Popen(command, shell=False, stdout=subprocess.PIPE).communicate()[0]
+        table_metadata = json.loads(output)
 
-    builder = BqQueryBuilder('taboola-data.pageviews.pageviews_20190120')
-    builder.buildColumnSizeQueries(columns)
+        ## parse json Schema
 
-    bqExecuter = BqExecuter()
-    for column in columns:
-        size = bqExecuter.execute(column._queries['columnSizeQuery'])
-        column.addProperty('size_bytes', size)
+        schemaParser = JsonSchemaParser('pageview-simple-json-schema.json')
+        schema = schemaParser.parse()
+
+        ## prepare column list based on cmd line args and pass them to a class to:
+        ## this class creates queries, execute and builds output dictionary(?)
+
+        # analyze a single column name (fully qualified) with childs
+        # for key, value in schemaParser.column_name_dictionary.iteritems():
+        #     if key.startswith('pv.requests.servedItems'):
+        #         pass
+
+        # prepare list of columns
+        columns = map(lambda (name, col): ColumnMetadata(name, col), schemaParser.column_name_dictionary.iteritems())
+
+        builder = BqQueryBuilder('taboola-data.pageviews.pageviews_20190120')
+        builder.buildColumnSizeQueries(columns)
+
+        bqExecuter = BqExecuter()
+        for column in columns:
+            size = bqExecuter.execute(column._queries['columnSizeQuery'])
+            column.addProperty('size_bytes', size)
 
     # schemaField = schemaParser.column_name_dictionary['pv.requests.postAuctionEvent.postAuctionData.auctionBidData.lost.bid']
     #
@@ -95,8 +125,41 @@ def main():
 ## the class creates instructions based on the schema
 ## the class execute the commands (or just print them)
 ## the class builds an output from the table & executed commands (per field: name, parent, field size, etc..
+## format output
+## load to BQ
 
 
+def parseArgs():
+
+    parent_parser_1 = argparse.ArgumentParser(add_help=False)
+    parent_parser_1.add_argument('-f', '--from-date', type=str, dest='from_date', help='First day of table. default: yesterday')
+    parent_parser_1.add_argument('-t', '--to-date', type=str, dest='to_date', help='Last day of table. default: from_date')
+    parent_parser_1.add_argument('-p', '--project', type=str, dest='project', default='taboola-data', help='default: taboola-data')
+    parent_parser_1.add_argument('-d', '--dataset', type=str, dest='dataset', default='pageviews', help='default: pageviews')
+    parent_parser_1.add_argument('-n', '--daily-table-prefix', type=str, dest='table_prefix', default='pageviews', help='daily table prefix name. default: pageviews')
+
+    parent_parser_2 = argparse.ArgumentParser(add_help=False)
+    parent_parser_2.add_argument('-c', '--column-name', type=str, dest='column_name', help='')
+
+    parser = argparse.ArgumentParser(description='bq-table-metadata-analyzer')
+    subparsers = parser.add_subparsers(dest="command", title='sub-commands',help='sub-commands help')
+    subparsers.add_parser('full-scan', parents=[parent_parser_1], help='full-scan help')
+    subparsers.add_parser('column-name-scan', parents=[parent_parser_1, parent_parser_2], help='column-name-scan help')
+    subparsers.add_parser('column-name-deep-scan', parents=[parent_parser_1, parent_parser_2], help='column-name-deep-scan help')
+    level_parser = subparsers.add_parser('column-level-scan', parents=[parent_parser_1], help='column-level-scan help')
+    level_parser.add_argument('-l', '--level', required=True, type=int, dest='level', default='', help='nesting level from 0. default: 0')
+
+    type_parser = subparsers.add_parser('column-type-scan', parents=[parent_parser_1], help='column-type-scan help')
+    type_parser.add_argument('-y', '--type', required=True, type=str, dest='column_type', help='column type. see: https://cloud.google.com/bigquery/docs/schemas#standard_sql_data_types')
+
+    mode_parser = subparsers.add_parser('column-mode-scan', parents=[parent_parser_1], help='column-mode-scan help')
+    mode_parser.add_argument('-m', '--mode', required=True, type=str, choices=['NULLABLE', 'REQUIRED', 'REPEATED'], dest='column_mode', help='column mode. see: https://cloud.google.com/bigquery/docs/schemas#modes')
+
+    subparsers.add_parser('lists-scan', parents=[parent_parser_1], help='lists-scan help')
+
+    args = parser.parse_args()
+    print args
+    return args
 
 if __name__ == '__main__':
     main()
